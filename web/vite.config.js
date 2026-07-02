58b100e9985a5054d90cd7387b8e7bc923a4faf9
Filipe Vieira <sv.filipe@gmail.com>
2026-07-02 16:31:23 +0100

    fix config

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,3 +1,4 @@
+/*
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -8,4 +9,20 @@ export default defineConfig({
     port: Number(process.env.PORT) || 3000,
     strictPort: true
   }
+});*/
+
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    base: env.BASE || "/",
+    server: {
+      port: Number(env.PORT) || 3000,
+      strictPort: true,
+    },
+  };
 });
