b409123870a0493df26a2db56ef9dcdb21cdc426
Filipe Vieira <sv.filipe@gmail.com>
2026-07-02 16:16:59 +0100

    add base

--- web/vite.config.js
+++ web/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.BASE || '/',
   server: {
     port: Number(process.env.PORT) || 3000,
     strictPort: true
